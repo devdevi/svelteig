6fd00c9106c7559484b4ddc277e1df60025b5747
Visaka Devi <visaka.chaves@consorcio.cl>
2020-05-11 08:01:51 -0400

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,76 @@
+const webpack = require('webpack');
 const path = require('path');
+const config = require('sapper/config/webpack.js');
+const pkg = require('./package.json');
+
+const mode = process.env.NODE_ENV;
+const dev = node === 'development';
+
+const alias = { svelte: path.resolve('node_modules', 'svelte') }
+const extensions = ['.mjs', '.js', '.json', '.svelte', '.html']
+const mainFields = ['svelte', 'module', 'browser', 'main']
+
+module.exports = {
+    client: {
+        entry: config.client.entry(),
+        output: config.client.output(),
+        resolve: { alias, extensions, mainFields },
+        module: {
+            rules: [
+                {
+                    test: /\.(svelte|html)$/,
+                    use: {
+                        loader: 'svelte-loader',
+                        options: {
+                            dev,
+                            hydrateble: true,
+                            hotReload: false,
+                        }
+                    }
+                }
+            ]
+        },
+        mode,
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.browser': true,
+                'procces.env.NODE_EN': JSON.stringify(mode)
+            }),
+        ].filter(Boolean),
+        devtool: dev && 'inline-source-map'
+    },
+
+    server: {
+        entry: config.server.entry(),
+        output: config.server.output(),
+        target: 'node',
+        resolve: {
+            alias,
+            extensions,
+            mainFields
+        },
+        externals: Object.keys(pkg.dependencies).concat('encoding'),
+        module: {
+            rules: [
+                {
+                    test: /\.(svelte|html)$/,
+                    use: {
+                        loader: 'svelte-loader',
+                        options: {
+                            dev,
+                            css: false,
+                            generate: 'ssr'
+                        }
+                    }
+                }
+            ]
+        },
+        mode: process.env.NODE_ENV,
+        performance: {
+            hints: false
+        }
+    }
+}/*
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -38,4 +110,4 @@ module.exports = {
             filename: './index.html'
         })
     ]
-}
+} */
