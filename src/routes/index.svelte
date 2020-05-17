bb8fb4d68cc50846de41f3db86a87b5f741e80f0
Visaka Devi <visaka.chaves@consorcio.cl>
2020-05-17 17:21:14 -0400

    Primer llamado con prefetch

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,30 +1,44 @@
+<script context="module">
+  export async function preload() {
+    let data = {};
+    const API = "https://us-central1-pugstagram-co.cloudfunctions.net/data";
+    const response = await this.fetch(API);
+    data = await response.json();
+    return { data };
+  }
+</script>
+
 <script>
   /* Utils */
-  import { onMount } from "svelte";
+  /*   import { onMount } from "svelte"; */
   // Components
   import Main from "../components/Main.svelte";
 
   import Sidebar from "../components/Sidebar.svelte";
   import TimeLine from "./../components/TimeLine.svelte";
 
-  let data = {};
+  export let data;
+
+  /*   let data = {};*/
   const localUser = {
     user: {
       name: "Visaka Devi",
       nickname: "Visidevi"
     }
   };
-  const API = "https://us-central1-pugstagram-co.cloudfunctions.net/data";
-  onMount(async () => {
+  /*   onMount(async () => {
     const response = await fetch(API);
     data = await response.json();
-  });
+  }); */
 </script>
 
+<!-- Modulo -->
+<svelte:head>
+  <title>InstagramSvelte</title>
+
+</svelte:head>
 
 <Main>
   <TimeLine posts={data.posts} />
   <Sidebar {...localUser.user} />
 </Main>
-
-
